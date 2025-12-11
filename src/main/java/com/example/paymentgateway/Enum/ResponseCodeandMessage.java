c5cc91a5d1e1c15d0653cbbbeb26027266a1b240
eswarprasath <eswarprasath10@gmail.com>
2025-12-12 01:17:28 +0530

    commit the changes

--- src/main/java/com/example/paymentgateway/Enum/ResponseCodeandMessage.java
+++ src/main/java/com/example/paymentgateway/Enum/ResponseCodeandMessage.java
@@ -1,8 +1,11 @@
 package com.example.paymentgateway.Enum;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+
 public enum ResponseCodeandMessage {
     SUCCESS("0000","SUCCESS"),
     EMAILALREADYEXIT("0080","email address already exit");
